5767365f5f67d375e465c2fcd5946f5bdc0ed97a
Andrei Frankovic <frankovica@rsd407.org>
2025-04-25 08:04:01 -0700

    Finished

---
+++ MyGame/MeteorSpawner.cs
@@ -0,0 +1,41 @@
+﻿using GameEngine;
+
+using System;
+using SFML.System;
+namespace MyGame
+{
+    class MeteorSpawner : GameObject
+    {
+        // the number of milliseconds between meteor spans.
+        private const int SpawnDelay = 1000;
+        private int _timer;
+        public override void Update(Time elapsed)
+        {
+            // Determine how much time has passed and adjust our timer.
+            int msElapsed = elapsed.AsMilliseconds();
+            _timer -= msElapsed;
+            // If our timer has elapsed, reset it and spawn a meteor.
+            if (_timer <= 0)
+            {
+                _timer = SpawnDelay;
+                Vector2u size = Game.RenderWindow.Size;
+                // Spawn the meteor off the right side of the screen.
+                // We're assuming the meteor isn't more than 100 pixels wide.
+                float minY = Math.Max(10, size.Y * 0.1f);
+                float maxY = Math.Min(size.Y - 10, size.Y * .09f);
+                if (minY >= maxY)
+                {
+                    minY = size.Y * 0.25f;
+                    maxY = size.Y * 0.75f;
+                }
+                float meteorX = size.X + 100;
+                // Spawn the meteor somewhere aloing the height of the window, randomly.
+                float meteorY = Game.Random.Next((int)minY, (int)maxY);
+               
+                // Create a meteor and add it to the scene
+                Meteor meteor = new Meteor(new Vector2f(meteorX, meteorY));
+                Game.CurrentScene.AddGameObject(meteor);
+            }
+        }
+    }
+}
---
+++ MyGame/ScrollingBackground.cs
@@ -0,0 +1,46 @@
+﻿using GameEngine;
+using SFML.Graphics;
+using SFML.System;
+
+namespace MyGame
+{
+    class ScrollingBackground : GameObject
+    {
+        private Sprite _sprite;
+        private Texture _texture;
+        private float _scrollSpeed = 0.1f; // pixels per millisecond
+
+        public ScrollingBackground()
+        {
+            _texture = Game.GetTexture("Resources/background.png");
+            _sprite = new Sprite(_texture);
+            _sprite.Position = new Vector2f(0, 0);
+        }
+
+        public override void Update(Time elapsed)
+        {
+            float moveX = _scrollSpeed * elapsed.AsMilliseconds();
+            _sprite.Position = new Vector2f(_sprite.Position.X - moveX, _sprite.Position.Y);
+
+            // Reset to 0 when background fully scrolled off-screen
+            if (_sprite.Position.X <= -_texture.Size.X)
+            {
+                _sprite.Position = new Vector2f(0, _sprite.Position.Y);
+            }
+        }
+
+        public override void Draw()
+        {
+            // Draw main background
+            Game.RenderWindow.Draw(_sprite);
+
+            // Draw second background next to it for smooth looping
+            if (_sprite.Position.X < 0)
+            {
+                Sprite secondSprite = new Sprite(_texture);
+                secondSprite.Position = new Vector2f(_sprite.Position.X + _texture.Size.X, _sprite.Position.Y);
+                Game.RenderWindow.Draw(secondSprite);
+            }
+        }
+    }
+}
